04a4867d6491df773d6a0a9edecf646a0f78edd8
David Westreicher <david.westreicher@gmail.com>
2015-10-02 01:03:15 +0200

    add android vibration

--- android/src/com/westreicher/birdsim/android/AndroidLauncher.java
+++ android/src/com/westreicher/birdsim/android/AndroidLauncher.java
@@ -7,11 +7,12 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.westreicher.birdsim.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useImmersiveMode = true;
-		initialize(new MyGdxGame(), config);
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
+        initialize(new MyGdxGame(), config);
+    }
 }
